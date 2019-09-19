b6bd2e429402f6b02fef3833ae5b94f4f0c2bca2
Michael Fornaro <Michael.Fornaro@anz.com>
2019-09-20 08:24:14 +1000

    Register v1alpha3 type with the controller

--- certificate-operator/pkg/apis/addtoscheme_app_v1alpha1.go
+++ certificate-operator/pkg/apis/addtoscheme_app_v1alpha1.go
@@ -2,9 +2,11 @@ package apis
 
 import (
 	"github.com/xUnholy/k8s-operator/pkg/apis/app/v1alpha1"
+	"knative.dev/pkg/apis/istio/v1alpha3"
 )
 
 func init() {
 	// Register the types with the Scheme so the components can map objects to GroupVersionKinds and back
 	AddToSchemes = append(AddToSchemes, v1alpha1.SchemeBuilder.AddToScheme)
+	AddToSchemes = append(AddToSchemes, v1alpha3.SchemeBuilder.AddToScheme)
 }
