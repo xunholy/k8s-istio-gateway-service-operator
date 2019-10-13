0f3b3801146440c47731a229340c59bc61d2c10e
Michael Fornaro <Michael.Fornaro@anz.com>
2019-10-14 10:00:25 +1100

    Remove redundant tests

--- certificate-operator/internal/pkg/validate/base64.go
+++ certificate-operator/internal/pkg/validate/base64.go
@@ -7,17 +7,17 @@ import (
 	"github.com/xUnholy/k8s-operator/pkg/apis/app/v1alpha1"
 )
 
-func CheckBase64(data string) bool {
+func checkBase64(data string) bool {
 	_, err := base64.StdEncoding.DecodeString(data)
 	return err == nil
 }
 
 func ValidateSecretEncoding(TLSSecret v1alpha1.TLSSecret) error {
-	valid := CheckBase64(*TLSSecret.Cert)
+	valid := checkBase64(*TLSSecret.Cert)
 	if !valid {
 		return fmt.Errorf("cert is not valid base64 encoded")
 	}
-	valid = CheckBase64(*TLSSecret.Key)
+	valid = checkBase64(*TLSSecret.Key)
 	if !valid {
 		return fmt.Errorf("key is not valid base64 encoded")
 	}
