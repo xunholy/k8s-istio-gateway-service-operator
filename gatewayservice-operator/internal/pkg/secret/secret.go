0f6626a6d395b541d6a2239932c6b7ec7634498f
Michael Fornaro <dreamappsoftware@gmail.com>
2019-10-17 19:17:02 +1100

    Refactor and update documentation (#9)
    * Refactoring k8s Kind
    
    * Refactoring documentation
    
    * Update CI to reflect refactor changes
    
    * Refactoring documentation and updating kinds
    
    * Fix testing after Server.Port.Name was made unique with the namespace identifier
    
    * Removing duplicate files
    
    * Update CI pipeline to correct refactoring

---
+++ gatewayservice-operator/internal/pkg/secret/secret.go
@@ -0,0 +1,38 @@
+package secret
+
+import (
+	appv1alpha1 "github.com/xUnholy/k8s-operator/pkg/apis/app/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type SecretConfig struct {
+	Name        string
+	Namespace   string
+	Labels      map[string]string
+	Certificate *appv1alpha1.GatewayService
+}
+
+func Reconcile(s SecretConfig) *corev1.Secret {
+	return &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      s.Name,
+			Namespace: s.Namespace,
+			Labels:    s.Labels,
+		},
+		// Data contains the secret data. Each key must consist of alphanumeric
+		// characters, '-', '_' or '.'. The serialized form of the secret data is a
+		// base64 encoded string, representing the arbitrary (possibly non-string)
+		// data value here. Described in https://tools.ietf.org/html/rfc4648#section-4
+		Data: map[string][]byte{
+			"tls.key": []byte(*s.Certificate.Spec.TLSOptions.TLSSecret.Key),
+			"tls.crt": []byte(*s.Certificate.Spec.TLSOptions.TLSSecret.Cert),
+		},
+		// Used to facilitate programmatic handling of secret data.
+		Type: "kubernetes.io/tls",
+	}
+}
---
+++ gatewayservice-operator/internal/pkg/secret/secret_test.go
@@ -0,0 +1,58 @@
+package secret_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	s "github.com/xUnholy/k8s-operator/internal/pkg/secret"
+	appv1alpha1 "github.com/xUnholy/k8s-operator/pkg/apis/app/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	name      = "example-app"
+	namespace = "application"
+	cert      = "Q2VydAo="
+	key       = "S2V5Cg=="
+)
+
+func TestSecretReconcile(t *testing.T) {
+	certificate := &appv1alpha1.GatewayService{
+		Spec: appv1alpha1.GatewayServiceSpec{
+			TLSOptions: &appv1alpha1.TLSOptions{
+				TLSSecret: &appv1alpha1.TLSSecret{
+					Cert: &cert,
+					Key:  &key,
+				},
+			},
+		},
+	}
+	expected := &corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-%s-secret", name, namespace),
+			Namespace: namespace,
+			Labels:    map[string]string{"Namespace": namespace},
+		},
+		Data: map[string][]byte{
+			"tls.crt": []byte(cert),
+			"tls.key": []byte(key),
+		},
+		Type: "kubernetes.io/tls",
+	}
+	secretConfig := s.SecretConfig{
+		Name:        fmt.Sprintf("%s-%s-secret", name, namespace),
+		Namespace:   namespace,
+		Labels:      map[string]string{"Namespace": namespace},
+		Certificate: certificate,
+	}
+	secretObject := s.Reconcile(secretConfig)
+	if !reflect.DeepEqual(secretObject, expected) {
+		t.Fatalf("Expected: (%+v)\n Found: (%+v)", expected, secretObject)
+	}
+}
